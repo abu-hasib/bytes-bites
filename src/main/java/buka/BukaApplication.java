04b7eb2b8df7cfd8227d51a9628cdfb1803e5f97
Ridwan <ricklick262@gmail.com>
2021-05-27 16:58:24 +0100

    one-to-many, many-to-one

--- src/main/java/buka/BukaApplication.java
+++ src/main/java/buka/BukaApplication.java
@@ -1,13 +1,39 @@
 package buka;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import buka.model.City;
+import buka.model.Meal;
+import buka.model.ServiceProvider;
+import buka.repository.CityRepository;
+import buka.repository.ServiceProviderRepository;
 
 @SpringBootApplication
 public class BukaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BukaApplication.class, args);
+		ConfigurableApplicationContext configurableApplicationContext = SpringApplication.run(BukaApplication.class,
+				args);
+ServiceProviderRepository serviceProviderRespository = configurableApplicationContext
+				.getBean(ServiceProviderRepository.class);
+CityRepository cityRepository = configurableApplicationContext
+				.getBean(CityRepository.class);
+
+		City city = new City("Lagos");
+		ServiceProvider provider = new ServiceProvider("Fingers", "mail@mail.org", 399399394, city);
+		List<ServiceProvider> serviceProviders = Arrays.asList(provider);
+		city.setServiceProviders(serviceProviders);
+		cityRepository.save(city);
+		Meal rice = new Meal(20, "food", 15, provider);
+		Meal efo = new Meal(20, "good food", 10, provider);
+		List<Meal> meals = Arrays.asList(rice, efo);
+		provider.setMeals(meals);
+		serviceProviderRespository.save(provider);
 	}
 
 }
--- src/main/java/buka/BukaController.java
+++ src/main/java/buka/BukaController.java
@@ -7,17 +7,20 @@ import org.springframework.web.bind.annotation.RestController;
 
 import buka.model.ServiceProvider;
 import buka.repository.BukaRepository;
+import buka.repository.ServiceProviderRepository;
 
 @RestController
 public class BukaController {
 	private final BukaRepository repository;
+	private final ServiceProviderRepository sprepository;
 
-	public BukaController(BukaRepository repository) {
+	public BukaController(BukaRepository repository, ServiceProviderRepository sprepository) {
 		this.repository = repository;
+		this.sprepository = sprepository;
 	}
 	
 	@GetMapping("/bukas")
 	List<ServiceProvider> all() { 
-		return repository.findAll();
+		return sprepository.findAll();
 	}
 }
